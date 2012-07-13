448c50a34ab67036a5437121f435a2424e700f52
Kevin Schaaf <kevin.schaaf@palm.com>
2012-07-13 12:48:55 -0700

    Added iOS and Android phonegap projects.

---
+++ projects/Android/src/com/enyojs/sampler/EnyoSamplerAndroidActivity.java
@@ -0,0 +1,13 @@
+package com.enyojs.sampler;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class EnyoSamplerAndroidActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
