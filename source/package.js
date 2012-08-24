ffb5ded66c0469d5ad239570a5b12d77dc672f91
Kevin Schaaf <kevin.schaaf@palm.com>
2012-08-24 10:07:02 -0700

    ENYO-891, ENYO-677: Added sampler extras: test mode, side-loading samples, loading alternate source paths, and some refactoring. Enyo-DCO-1.0-Signed-off-by: Kevin Schaaf <kevin.schaaf@palm.com>

--- source/package.js
+++ source/package.js
@@ -1,12 +1,11 @@
 enyo.depends(
 	"$lib/layout",
+	"$lib/onyx",
 	"$lib/layout/fittable/samples",
 	"$lib/layout/panels/samples",
 	"$lib/layout/list/samples",
 	"$lib/layout/tree",
 	"$lib/layout/tree/samples",
-	"$lib/onyx",
-	"$lib/onyx/source/wip-package.js",
 	"$lib/onyx/samples",
 	"$lib/canvas",
 	"$lib/canvas/samples",
