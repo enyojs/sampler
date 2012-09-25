c3df537cc7b4b13a648d1411ec920e17f9111b3c
Kevin Schaaf <kevin.schaaf@palm.com>
2012-09-24 18:03:31 -0700

    ENYO-1042: Created Android-nightly project.

---
+++ projects/Android-nightly/src/com/enyojs/sampler/nightly/EnyoSamplerAndroidActivity.java
@@ -0,0 +1,13 @@
+package com.enyojs.sampler.nightly;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class EnyoSamplerAndroidActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("http://nightly.enyojs.com/latest/sampler/index.html");
+    }
+}
