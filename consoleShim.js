f1f622cae4974aa93a8d665a498b1de5cf9db8c0
Ben Combee <ben.combee@lge.com>
2013-06-13 01:30:12 -0500

    Changes to support testing when enyo branch checked out to delay-enyo-kind
    Enyo-DCO-1.1-Signed-Off-By: Ben Combee (ben.combee@lge.com)

--- consoleShim.js
+++ consoleShim.js
@@ -1,18 +1,24 @@
 /* global console:true */
 (function() {
 	var buffer = "";
+	var originalLog = console.log;
+	var originalWarn = console.warn;
+	var originalError = console.error;
 
 	console.log = function() {
 		var a$ = Array.prototype.slice.call(arguments);
-		buffer += a$.join(" ") + "\r\n";
+		buffer += Date.now() + " " + a$.join(" ") + "\r\n";
+		originalLog.apply(console, arguments);
 	};
 	console.warn = function() {
 		var a$ = Array.prototype.slice.call(arguments);
-		buffer += "WARNING: " + a$.join(" ") + "\r\n";
+		buffer += Date.now() + " WARNING: " + a$.join(" ") + "\r\n";
+		originalWarn.apply(console, arguments);
 	};
 	console.error = function() {
 		var a$ = Array.prototype.slice.call(arguments);
-		buffer += "ERROR: " + a$.join(" ") + "\r\n";
+		buffer += Date.now() + " ERROR: " + a$.join(" ") + "\r\n";
+		originalError.apply(console, arguments);
 	};
 	console.getBuffer = function() {
 		return buffer;
